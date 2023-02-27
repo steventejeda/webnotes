fdbf7eae5124770a48c1ecd0628c4a1172eb3191
computer <computer@COMPUTER49CE>
2023-02-26 21:19:40 -0500

    database setup using Microsoft server management studio

---
+++ WebNotes/Data/NotesDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebNotes.Models.Entities;
+
+namespace WebNotes.Data
+{
+    public class NotesDbContext : DbContext
+    {
+        public NotesDbContext(DbContextOptions options) : base(options) 
+        {
+        } 
+
+        public DbSet<Note> Notes { get; set; }
+
+    }
+}
